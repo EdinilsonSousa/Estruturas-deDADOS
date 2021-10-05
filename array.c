0dd3e17f91d2d1b65a6eb6a294687ed90e721b30
EDi <60969430+EdinilsonSousa@users.noreply.github.com>
2021-10-05 09:23:04 -0300

    acessando seus valores por índice

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+  int num[5] = {2,1,4,3,5};
+  
+  printf("Exibindo os Valores do Vetor \n\n");
+  printf("num[0] = %d\n", num[0]);
+  printf("num[1] = %d\n", num[1]);
+  printf("num[2] = %d\n", num[2]);
+  printf("num[3] = %d\n", num[3]);
+  printf("num[4] = %d\n", num[4]);
+  
+  
+  return 0;
+}
